627c19a2c27a019e3bb7acee986dad9675b0de9e
Shaun <shaun@thenetninja.co.uk>
2021-03-30 16:21:59 +0100

    lesson-6

--- components/Navbar.js
+++ components/Navbar.js
@@ -15,8 +15,9 @@ export default function Navbar() {
         <ul>
           <li><Link href="/"><a>Home</a></Link></li>
           <li><Link href="/guides"><a>Guides</a></Link></li>
-          <li onClick={login} className="btn">Login/Signup</li>
-          <li onClick={logout} className="btn">Logout</li>
+          {!user && <li onClick={login} className="btn">Login/Signup</li>}
+          {user && <li>{user.email}</li>}
+          {user && <li onClick={logout} className="btn">Logout</li>}
         </ul>
       </nav>
       <div className="banner">
