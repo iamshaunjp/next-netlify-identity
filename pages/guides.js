ca4ec2c17e196be754c4d98adaa4894c22edffb0
Shaun <shaun@thenetninja.co.uk>
2021-03-31 10:37:59 +0100

    lesson-10

--- pages/guides.js
+++ pages/guides.js
@@ -1,9 +1,11 @@
-import { useContext, useEffect } from 'react'
+import { useContext, useEffect, useState } from 'react'
 import styles from '../styles/Guides.module.css'
 import AuthContext from '../stores/authContext'
 
 export default function Guides() {
-  const { user, authReady } = useContext(AuthContext)
+  const { user, authReady, login } = useContext(AuthContext)
+  const [guides, setGuides] = useState(null)
+  const [error, setError] = useState(null)
 
   useEffect(() => {
     if (authReady) {
@@ -12,15 +14,44 @@ export default function Guides() {
           Authorization:  'Bearer ' + user.token.access_token
         }
       })
-      .then(res => res.json())
-      .then(data => console.log(data))
+      .then(res => {
+        if (!res.ok) {
+          login()
+          throw Error('You must be logged in to view this content')
+        }
+        return res.json()
+      })
+      .then(data => {
+        setError(null)
+        setGuides(data)
+      })
+      .catch(err => {
+        setError(err.message)
+        setGuides(null)
+      })
     }
 
   },[user, authReady])
 
   return (
     <div className={styles.guides}>
-      <h2>All Guides</h2>
+      
+      {!authReady && <div>Loading...</div>}
+
+      {error && (
+        <div className={styles.error}>
+          <p>{ error }</p>
+        </div>
+      )}
+
+      {guides && guides.map(guide => (
+        <div key={guide.title} className={styles.card}>
+          <h3>{ guide.title }</h3>
+          <h4>written by {guide.author}</h4>
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. At corrupti iste ab magnam dignissimos id maxime rerum quae minima. Delectus maxime culpa est consequatur veritatis, perspiciatis cum corrupti possimus quis?</p>
+        </div>
+      ))}
+
     </div> 
   )
 }
