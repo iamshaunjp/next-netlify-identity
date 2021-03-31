ff19ba149272bac4fd86fe087b032ab3ea8bbabe
Shaun <shaun@thenetninja.co.uk>
2021-03-31 08:02:56 +0100

    lesson-8

--- pages/guides.js
+++ pages/guides.js
@@ -1,6 +1,14 @@
+import { useEffect } from 'react'
 import styles from '../styles/Guides.module.css'
 
 export default function Guides() {
+
+  useEffect(() => {
+    fetch('/.netlify/functions/supermario')
+      .then(res => res.json())
+      .then(data => console.log(data))
+  },[])
+
   return (
     <div className={styles.guides}>
       <h2>All Guides</h2>
